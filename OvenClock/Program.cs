569234846f6ba0c5414fedd0b1d18f71d8396d88
남지원 <jwnam@crevis.co.kr>
2023-09-13 09:49:08 +0900

    2525 : 오븐시계

---
+++ OvenClock/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OvenClock
+{
+	class Program
+	{
+		static string[] _time = new string[2];
+		static double _min;
+
+		static DateTime _nowTime = new DateTime();
+		static DateTime _cookedTime = new DateTime();
+
+		static void Main(string[] args)
+		{
+			// 시, 분을 입력받는다.
+			string time = Console.ReadLine();
+			_time = time.Split(' ');
+
+			// 요리하는데 필요한 분을 입력받는다.
+			string cooktime = Console.ReadLine();
+			_min = double.Parse(cooktime);
+
+			// 시 분에 요리하는데 필요한 시간을 더한다.
+			_nowTime = DateTime.Parse($"2023/09/13 {_time[0]}:{_time[1]}");
+			_nowTime = _nowTime.AddMinutes(_min);
+
+			// 더한 시간을 출력한다.
+
+			Console.WriteLine($"{_nowTime.Hour} {_nowTime.Minute}");
+
+		}
+	}
+}
