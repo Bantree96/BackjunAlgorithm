a0e43fed28779a402d0b946de801e79afa369483
남지원 <jwnam@crevis.co.kr>
2023-09-15 09:27:42 +0900

    2739 구구단

---
+++ gugudan/gugudan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gugudan
+{
+	class gugudan
+	{
+		static void Main(string[] args)
+		{
+			int number = int.Parse(Console.ReadLine());
+
+			for(int i = 1; i<=9; i++)
+			{
+				Console.WriteLine($"{number} * {i} = {number * i}");
+			}
+		}
+	}
+}
