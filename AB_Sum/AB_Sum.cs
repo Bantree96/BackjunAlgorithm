f8aab0fe0f8634a29f8fcd5d8da8a9530f9c9767
남지원 <jwnam@crevis.co.kr>
2023-09-18 10:05:22 +0900

    10950 : A+B 3

---
+++ AB_Sum/AB_Sum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AB_Sum
+{
+	class AB_Sum
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+
+			string[] readLine = new string[2];
+			List<int> a = new List<int>();
+			List<int> b = new List<int>();
+
+			// 입력
+			for(int i=0; i<count; i++)
+			{
+				readLine = Console.ReadLine().Split(' ');
+
+				a.Add(int.Parse(readLine[0]));
+				b.Add(int.Parse(readLine[1]));
+
+			}
+
+			// 출력
+			for(int i=0; i<count; i++)
+			{
+				Console.WriteLine($"{a[i] + b[i]}");
+			}
+		}
+	}
+}
