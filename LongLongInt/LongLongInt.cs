aea3928a54d3e726836f2924428bec80cd3232c9
남지원 <jwnam@crevis.co.kr>
2023-09-20 09:41:33 +0900

    25314 : longlongint

---
+++ LongLongInt/LongLongInt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongLongInt
+{
+	class LongLongInt
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			int count = n / 4;
+
+			for(int i = 0; i<count; i++)
+			{
+				Console.Write("long ");
+			}
+
+			Console.Write("int");
+		}
+	}
+}
