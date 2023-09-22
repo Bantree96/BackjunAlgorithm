2cb1d6a54f61ba01babe71b8ee0875ef61029d2d
남지원 <jwnam@crevis.co.kr>
2023-09-22 10:57:03 +0900

    11021 : A+B - 7

---
+++ AB_Sum2/AB_Sum2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace AB_Sum2
+{
+	class AB_Sum2
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+			string[] numbers = new string[2];
+			StringBuilder sb = new StringBuilder();
+
+			for(int i=1; i<=count; i++)
+			{
+				numbers = Console.ReadLine().Split(' ');
+				sb.AppendLine($"Case #{i}: {int.Parse(numbers[0]) + int.Parse(numbers[1])}");
+			}
+
+			Console.WriteLine(sb.ToString());
+		}
+	}
+}
