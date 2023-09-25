94847ab4898d8907df2487bceaccf644e7bd04f1
남지원 <jwnam@crevis.co.kr>
2023-09-25 10:06:22 +0900

    2439 : 별2

---
+++ Star2/Star2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace Star2
+{
+	class Star2
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+			int count2 = count;
+			StringBuilder sb = new StringBuilder();
+
+			for(int i=1; i<=count; i++)
+			{
+				for(int k=count2; k>1; k--)
+				{
+					sb.Append(' ');
+				}
+				for(int j=0; j<i; j++)
+				{
+					sb.Append('*');
+				}
+
+				count2 -= 1;
+				sb.AppendLine();
+			}
+
+			Console.Write(sb.ToString());
+
+		}
+	}
+}
