53c613ba0ad360be4e6c69ac0e616ee140746c3b
남지원 <jwnam@crevis.co.kr>
2023-09-25 11:21:23 +0900

    10807 : 개수 세기, LINQ가능

---
+++ CountCheck/CountCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CountCheck
+{
+	class CountCheck
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+			int wantCount = 0;
+			string[] inputs = new string[count];
+			inputs = Console.ReadLine().Split();
+
+			string want = Console.ReadLine();
+
+			for(int i=0; i<count; i++)
+			{
+				if(inputs[i] == want)
+				{
+					wantCount++;
+				}
+			}
+
+			//_ = Array.FindAll(inputs, x => x == want).Length;
+			Console.WriteLine(wantCount);
+		}
+	}
+}
