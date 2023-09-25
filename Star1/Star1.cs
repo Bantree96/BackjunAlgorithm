487a9a874874b56ba5cabd0c05b93bef4f5b1ccf
남지원 <jwnam@crevis.co.kr>
2023-09-25 09:32:14 +0900

    2438 : 별1

---
+++ Star1/Star1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Star1
+{
+	class Star1
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+			StringBuilder sb = new StringBuilder();
+			for(int i=1; i<=count; i++)
+			{
+				for (int j = 0; j < i; j++)
+				{
+					sb.Append('*');
+				}
+				sb.AppendLine();
+			}
+
+			Console.Write(sb.ToString());
+		}
+	}
+}
