78fbbad7c04e5f7d3ab2903cec17a7fc0850d35e
남지원 <jwnam@crevis.co.kr>
2023-09-25 10:36:37 +0900

    10952 : A + B 5 10951 : A + B 4
    이제부터 EOF 체크하기

---
+++ AB_Sum4/AB_Sum4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace AB_Sum4
+{
+	class AB_Sum4
+	{
+		static void Main(string[] args)
+		{
+			string inputs;
+			string[] inputArray = new string[2];
+			StringBuilder sb = new StringBuilder();
+			while (true)
+			{
+				inputs = Console.ReadLine();
+				if (inputs == null) break;
+
+				inputArray = inputs.Split(' ');
+				int a = int.Parse(inputArray[0]);
+				int b = int.Parse(inputArray[1]);
+
+				sb.AppendLine($"{a + b}");
+			}
+
+			Console.Write(sb.ToString());
+		}
+	}
+}
---
+++ AB_Sum5/AB_Sum5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace AB_Sum5
+{
+	class AB_Sum5
+	{
+		static void Main(string[] args)
+		{
+			string[] value = new string[2];
+			StringBuilder sb = new StringBuilder();
+
+			while (true)
+			{
+				value = Console.ReadLine().Split(' ');
+				int a = int.Parse(value[0]);
+				int b = int.Parse(value[1]);
+
+				if (a == 0 && b == 0) break;
+
+				sb.AppendLine($"{a + b}");
+			}
+
+			Console.Write(sb.ToString());
+		}
+	}
+}
