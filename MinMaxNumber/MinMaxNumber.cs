994fbabfe94f4315207c35800f4f1f347ffcdab1
남지원 <jwnam@crevis.co.kr>
2023-09-26 09:09:16 +0900

    10818 : 최소최대

---
+++ MinMaxNumber/MinMaxNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace MinMaxNumber
+{
+	class MinMaxNumber
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+
+			string[] inputs = new string[count];
+			inputs = Console.ReadLine().Split(' ');
+			int[] numbers;
+			numbers = Array.ConvertAll(inputs, int.Parse);
+
+			Console.Write(numbers.Min());
+			Console.Write(' ');
+			Console.Write(numbers.Max());
+
+		}
+	}
+}
