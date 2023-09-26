fff4e99f7d0e64478193acf12df34e3b4d0b6c4d
남지원 <jwnam@crevis.co.kr>
2023-09-26 14:44:47 +0900

    5597 : 과제안내신분

---
+++ NotGiveHomework/NotGiveHomework.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NotGiveHomework
+{
+	class NotGiveHomework
+	{
+		static void Main(string[] args)
+		{
+			int[] studentNumbers = new int[30];
+			for(int i = 0; i < 28; i++)
+			{
+				int number = int.Parse(Console.ReadLine());
+				studentNumbers[number - 1] = 1;
+			}
+
+			for(int i = 0; i < 30; i++)
+			{
+				if(studentNumbers[i] == 0)
+				{
+					Console.WriteLine(i + 1);
+				}
+			}
+
+		}
+	}
+}
