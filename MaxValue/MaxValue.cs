5febc5c638420c4370970e7a876afa21d955d3b5
남지원 <jwnam@crevis.co.kr>
2023-09-26 09:16:56 +0900

    2562 : 최대값

---
+++ MaxValue/MaxValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace MaxValue
+{
+	class MaxValue
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = new int[9];
+			for(int i=0; i<9; i++)
+			{
+				numbers[i] = int.Parse(Console.ReadLine());
+			}
+
+			Console.WriteLine(numbers.Max());
+			Console.WriteLine(Array.FindIndex(numbers, x => x == numbers.Max())+1);
+		}
+	}
+}
