9733fc106658d27a792fdaf0420d4551a3af7337
남지원 <jwnam@crevis.co.kr>
2023-10-05 09:04:05 +0900

    2743 : 단어 길이 재기

---
+++ WordLength/WordLength.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WordLength
+{
+	class WordLength
+	{
+		static void Main(string[] args)
+		{
+			string text = Console.ReadLine();
+
+			Console.WriteLine(text.Length);
+		}
+	}
+}
