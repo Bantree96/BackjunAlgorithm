5ac0712a60fe52d6d22f6d0465b6e822af09ec5c
남지원 <jwnam@crevis.co.kr>
2023-10-05 09:35:55 +0900

    11720 : 숫자의 합

---
+++ NumberSum/NumberSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NumberSum
+{
+	/// <summary>
+	/// Char를 Int로 변환하는 방법이 다양한듯 하다.
+	/// </summary>
+	class NumberSum
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+			string numbers = Console.ReadLine();
+
+			int result = 0;
+			for(int i = 0; i < count; i++)
+			{
+				result += int.Parse(numbers[i].ToString());
+			}
+			Console.WriteLine(result);
+		}
+	}
+}
