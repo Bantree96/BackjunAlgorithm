3d02f3e2ec531412d4259c093f86d4e23be8eed1
남지원 <jwnam@crevis.co.kr>
2023-10-05 09:00:21 +0900

    27866 : 문자와 문자열

---
+++ CharWithString/CharWithString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CharWithString
+{
+	class CharWithString
+	{
+		static void Main(string[] args)
+		{
+			string text = Console.ReadLine();
+			int count = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(text[count-1]);
+		}
+	}
+}
