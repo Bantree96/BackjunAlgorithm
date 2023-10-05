d2b8152171c70b7f6c995894d852fdc878e1fd99
남지원 <jwnam@crevis.co.kr>
2023-10-05 09:27:29 +0900

    11654 : 아스키코드

---
+++ ASCIICode/ASCIICode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace ASCIICode
+{
+	class ASCIICode
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			if (input == null) return;
+
+			ASCIIEncoding ascii = new ASCIIEncoding();
+			byte[] text = ascii.GetBytes(input);
+
+			Console.WriteLine(text[0]);
+		}
+	}
+}
