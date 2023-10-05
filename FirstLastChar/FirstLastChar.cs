1e461bc961ca8663da442338f1b6a825fb098713
남지원 <jwnam@crevis.co.kr>
2023-10-05 09:14:03 +0900

    9086 : 문자열 첫번째 마지막번째 출력

---
+++ FirstLastChar/FirstLastChar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace FirstLastChar
+{
+	class FirstLastChar
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+			StringBuilder sb = new StringBuilder();
+
+			for(int i = 0; i < count; i++)
+			{
+				string text = Console.ReadLine();
+
+				sb.Append(text.First());
+				sb.Append(text.Last());
+				sb.AppendLine();
+			}
+
+			Console.WriteLine(sb.ToString());
+		}
+	}
+}
