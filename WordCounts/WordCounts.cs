43a14f7ace54187c601a81e3e73c8e64bc2e6dd1
남지원 <jwnam@crevis.co.kr>
2023-10-06 10:18:45 +0900

    1152 : 단어의 개수

---
+++ WordCounts/WordCounts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WordCounts
+{
+	class WordCounts
+	{
+		static void Main(string[] args)
+		{
+			
+			string inputs = Console.ReadLine();
+			string[] texts;
+			inputs = inputs.Trim();
+
+			texts = inputs.Split(' ');
+
+			if(texts.Length == 1 && texts[0].Equals(""))
+			{
+				Console.WriteLine(0);
+				return;
+			}
+
+			Console.WriteLine(texts.Length);
+		}
+	}
+}
