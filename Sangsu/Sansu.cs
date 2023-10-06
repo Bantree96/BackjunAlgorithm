7a8b53b2ccf162202951949eeea4912c378f38f5
남지원 <jwnam@crevis.co.kr>
2023-10-06 10:33:03 +0900

    2908 : 상수(사람이름임)

---
+++ Sangsu/Sansu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Sangsu
+{
+	/// <summary>
+	/// 상수는 숫자를 반대로 본다.
+	/// </summary>
+	class Sansu
+	{
+		static void Main(string[] args)
+		{
+			string[] inputs = Console.ReadLine().Split(' ');
+
+			int a = ReverseNumber(inputs[0]);
+			int b = ReverseNumber(inputs[1]);
+
+			if(a > b)
+			{
+				Console.WriteLine(a);
+			}
+			else
+			{
+				Console.WriteLine(b);
+			}
+		}
+		
+		/// <summary>
+		/// 문자열을 받아서 Reverse 후 int로 반환
+		/// </summary>
+		/// <param name="number">문자열</param>
+		/// <returns></returns>
+		static int ReverseNumber(string number)
+		{
+			var a = number.Reverse();
+			return int.Parse(string.Concat(a));
+		}
+	}
+}
