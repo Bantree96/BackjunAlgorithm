15be4590080fb598f665387dd6c85333f26379c4
남지원 <jwnam@crevis.co.kr>
2023-10-10 10:27:01 +0900

    11718 : 그냥출력하기

---
+++ JustReturn/JustReturn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace JustReturn
+{
+	class JustReturn
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				int count = 0;
+				while (count < 100)
+				{
+					string input = Console.ReadLine();
+
+					// 빈 줄인지 확인
+					if (input == null || input == "") return;
+
+					// 각 줄이 100글자를 넘지 않는지 확인
+					//if (input.Length > 100) throw new ArgumentOutOfRangeException();
+
+					// 공백으로 끝나지 않는다.
+					Console.WriteLine(input.Trim());
+
+					count++;
+				}
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+	}
+}
