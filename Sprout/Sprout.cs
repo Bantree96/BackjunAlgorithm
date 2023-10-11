b71b2eef6b8be1bd27d1b5b6dcd3194d1b3b8a23
남지원 <jwnam@crevis.co.kr>
2023-10-11 09:14:39 +0900

    새싹 : 25083

---
+++ Sprout/Sprout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Sprout : 새싹
+namespace Sprout
+{
+	class Sprout
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"         ,r'\"7 ");
+			Console.WriteLine($"r`-_   ,'  ,/ ");
+			Console.WriteLine($" \\. \". L_r' ");
+			Console.WriteLine($"   `~\\/ ");
+			Console.WriteLine($"      | ");
+			Console.WriteLine($"      | ");
+		}
+	}
+}
