f73e7fac8c7ac79d7c3d34b6ea818af1cc03f7ac
남지원 <jwnam@crevis.co.kr>
2023-10-12 11:26:35 +0900

    별찍기7

---
+++ Star7/Star7.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Star7
+{
+	class Star7
+	{
+		static void Main(string[] args)
+		{
+			int input = int.Parse(Console.ReadLine());
+
+			int starCount = 1;
+			int spaceCount = input - 1;
+			for(int i = 0; i < input; i++)
+			{
+				for(int j = 0; j < spaceCount; j++)
+				{
+					Console.Write(" ");
+				}
+				for (int s = 0; s < starCount; s++)
+				{
+					Console.Write("*");
+				}
+
+				Console.WriteLine();
+				
+				if(i == input - 1) { break; }
+
+				starCount += 2;
+				spaceCount -= 1;
+
+			}
+
+			for (int i = input -1; i > 0; i--)
+			{
+				starCount -= 2;
+				spaceCount += 1;
+
+				for (int j = 0; j < spaceCount; j++)
+				{
+					Console.Write(" ");
+				}
+				for (int s = 0; s < starCount; s++)
+				{
+					Console.Write("*");
+				}
+
+				Console.WriteLine();
+			}
+
+		}
+	}
+}
