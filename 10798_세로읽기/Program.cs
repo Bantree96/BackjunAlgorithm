7f0e878d500e821c21ae2cfcc311049a66d9b54f
남지원 <jwnam@crevis.co.kr>
2023-10-26 09:17:29 +0900

    10798 : 세로읽기

---
+++ 10798_세로읽기/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _10798_세로읽기
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[,] text = new string[5,15];
+			text.Initialize();
+
+			// 입력
+			for(int i = 0; i < 5; i++)
+			{
+				string input = Console.ReadLine();
+
+				for(int j = 0; j < input.Length; j++)
+				{
+					text[i, j] = input[j].ToString();
+				}
+			}
+
+
+			// 출력
+			for(int j = 0; j < 15; j++)
+			{
+				for(int i = 0; i < 5; i++)
+				{
+					if(text[i,j] == null)
+					{
+						continue;
+					}
+					else
+					{
+						Console.Write(text[i, j]);
+					}
+				}
+			}
+		}
+	}
+}
