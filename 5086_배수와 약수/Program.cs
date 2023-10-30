d7d46533ea8b40bc760b897b7633859625af9412
남지원 <jwnam@crevis.co.kr>
2023-10-30 10:09:21 +0900

    5086 : 배수와 약수

---
+++ 5086_배수와 약수/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _5086_배수와_약수
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				int[] inputs = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+				int a = inputs[0];
+				int b = inputs[1];
+
+				if(a == 0 && b == 0)
+				{
+					break;
+				}
+
+				int c = a % b;
+				if(c == a)
+				{
+					Console.WriteLine("factor");
+				}
+				else if(c == 0)
+				{
+					Console.WriteLine("multiple");
+				}
+				else
+				{
+					Console.WriteLine("neither");
+				}
+			}
+		}
+	}
+}
