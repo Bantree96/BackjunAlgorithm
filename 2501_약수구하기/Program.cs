90dc0e9dd91aac2d955d696a021221904e0be888
남지원 <jwnam@crevis.co.kr>
2023-10-30 10:43:41 +0900

    2501 : 약수 구하기

---
+++ 2501_약수구하기/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2501_약수구하기
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] inputs = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+			int number = inputs[0];
+			int index = inputs[1];
+
+			// divisor = 약수
+			List<int> divisors = new List<int>();
+			for(int i = number; i > 0; i--)
+			{
+				if(number % i == 0)
+				{
+					divisors.Add(i);
+				}
+			}
+			divisors.Sort();
+
+			// try-catch사용한것보다 메모리에 이점이 있음
+			if(divisors.Count < index)
+			{
+				Console.WriteLine(0);
+			}
+			else
+			{
+				Console.WriteLine(divisors[index - 1]);
+			}
+		}
+	}
+}
