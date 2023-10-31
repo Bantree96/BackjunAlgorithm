d5973f631364c63799d84bb5cb3906f4e0ad0ea3
남지원 <jwnam@crevis.co.kr>
2023-10-31 10:43:27 +0900

    9506 : 약수들의합

---
+++ 9506_약수들의합/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _9506_약수들의합
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				int number = int.Parse(Console.ReadLine());
+				if (number == -1) return;
+
+				// 약수 구하는 공식
+				List<int> divisors = new List<int>();
+				// 자기 자신을 제외한다.
+				for (int i = number-1; i > 0; i--)
+				{
+					if (number % i == 0)
+					{
+						divisors.Add(i);
+					}
+				}
+				divisors.Sort();
+				
+				// 출력
+				if (divisors.Sum() == number)
+				{
+					string divi = string.Join($" + ", divisors);
+					Console.WriteLine($"{number} = {divi}");
+				}
+				else
+				{
+					Console.WriteLine($"{number} is NOT perfect.");
+				}
+			}
+		}
+	}
+}
