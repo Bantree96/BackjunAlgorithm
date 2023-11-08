af29f0db440a66a02b0a9b575eb44ff1553c45ea
남지원 <jwnam@crevis.co.kr>
2023-11-08 12:22:23 +0900

    소수문제

---
+++ 1978_소수찾기/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _1978_소수찾기
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+			int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+			int primeNumberCount = 0;
+
+			for(int i = 0; i < count; i++)
+			{
+				if (IsPrimeNumber(numbers[i]))
+				{
+					primeNumberCount++;
+				}
+			}
+
+			Console.WriteLine(primeNumberCount);
+		}
+
+		static bool IsPrimeNumber(int number)
+		{
+			if (number == 1) return false;
+
+			for(int i = 2; i * i <= number; i++)
+			{
+				if(number % i == 0)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
---
+++ 2581_소수/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2581_소수
+{
+	class Program
+	{
+		static List<int> _primeSum = new List<int>();
+		static void Main(string[] args)
+		{
+			int number1 = int.Parse(Console.ReadLine());
+			int number2 = int.Parse(Console.ReadLine());
+
+			for (int i = number1; i <= number2; i++)
+			{
+				if (IsPrimeNumber(i))
+				{
+					_primeSum.Add(i);
+				}
+			}
+
+			if(_primeSum.Count == 0)
+			{
+				Console.WriteLine(-1);
+			}
+			else
+			{
+				Console.WriteLine(_primeSum.Sum());
+				Console.WriteLine(_primeSum.Min());
+			}
+		}
+
+		static bool IsPrimeNumber(int number)
+		{
+			if (number == 1) return false;
+
+			for (int i = 2; i * i <= number; i++)
+			{
+				if (number % i == 0)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
