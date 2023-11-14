a3bdf31a44a38e03be9eed17329d092e6f79b00d
남지원 <jwnam@crevis.co.kr>
2023-11-14 11:26:10 +0900

    11653 소인수분해

---
+++ 11653_소인수분해/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _11653_소인수분해
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			int number = int.Parse(Console.ReadLine());
+
+			if (number == 1) return;
+
+			for (int i = 2; i <= number; i++)
+			{
+				while (number % i == 0)
+				{
+					Console.WriteLine(i);
+					number /= i;
+				}
+			}
+
+		}
+
+	}
+}
