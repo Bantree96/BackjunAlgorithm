36059024c18ef4ed09897244e0fa04ff6d9b5ec1
남지원 <jwnam@crevis.co.kr>
2023-11-22 08:23:10 +0900

    15894_수학은체육과목 9063_대지

---
+++ 15894_수학은체육과목/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _15894_수학은체육과목
+{
+	/// <summary>
+	/// 입력이 1 <= n <= 10의 9승인데 10의9승을 넣으면 int.max보다 커 예외 발생
+	/// </summary>
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			long boxCount = int.Parse(Console.ReadLine());
+			long result = 0;
+
+			//for (int i = 0; i < boxCount; i++)
+			//{
+			//	result += 3;
+			//}
+
+			//result += boxCount;
+
+			result = 4 * boxCount;
+
+			Console.WriteLine(result);
+		}
+	}
+}
---
+++ 9063_대지/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// 좌표를 입력받아 직사각형 땅의 넓이를 구하는 문제
+/// </summary>
+namespace _9063_대지
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int dotCount = int.Parse(Console.ReadLine());
+			
+			List<int> x = new List<int>();
+			List<int> y = new List<int>();
+
+			// 옥구슬 좌표 입력
+			int[] coordinate = new int[2];
+			for (int i = 0; i< dotCount; i++)
+			{
+				coordinate = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+				x.Add(coordinate[0]);
+				y.Add(coordinate[1]);
+			}
+
+			// 점의 갯수가 부족해 계산 할 수 없을 때 
+			if(dotCount <= 1)
+			{
+				Console.WriteLine(0);
+				return;
+			}
+
+			// 사각형의 넓이 계산
+			int result = (x.Max() - x.Min()) * (y.Max() - y.Min());
+			Console.WriteLine(result);
+		}
+	}
+}
