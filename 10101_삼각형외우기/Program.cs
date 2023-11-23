5cd1c63ba81bd74712091473053bb32f7d489d60
남지원 <jwnam@crevis.co.kr>
2023-11-23 10:39:22 +0900

    10101_삼각형외우기

---
+++ 10101_삼각형외우기/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _10101_삼각형외우기
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] triangle = new int[3];
+			for(int i = 0; i < 3; i++)
+			{
+				triangle[i] = int.Parse(Console.ReadLine());
+			}
+
+			int angle = triangle[0] + triangle[1] + triangle[2];
+			if(triangle[0] == 60 && triangle[1] == 60 && triangle[2] == 60)
+			{
+				Console.WriteLine("Equilateral");
+				return;
+
+			}	
+			
+			if(angle == 180)
+			{
+				if(triangle[0] == triangle[1] || triangle[1] == triangle[2] || triangle[0] == triangle[2])
+				{
+					Console.WriteLine("Isosceles");
+					return;
+				}
+				
+				if(triangle[0] != triangle[1] && triangle[1] != triangle[2] && triangle[0] != triangle[2])
+				{
+					Console.WriteLine("Scalene");
+					return;
+				}
+			}
+			else
+			{
+				Console.WriteLine("Error");
+			}
+
+		}
+
+	}
+}
